1a2323a485c394063909069b5f12b56ab16ed17f
Matthew Vecchio <Matthew.Vecchio@gmail.com>
2018-04-01 22:12:52 +0930

    Added the shell of the Appointments page.
    The program will run and display the fields for Appointments. No programming done yet for the buttons.

---
+++ Start-Finance-master/InstaRichie/Models/Appointments.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    class Appointments
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [NotNull]
+        public string EventName { get; set; }
+
+     
+        public string Location { get; set; }
+
+        [NotNull]
+        public string EventDate { get; set; }
+
+       
+        public string StartTime { get; set; }
+
+     
+        public double EndTime { get; set; }
+
+    }
+}
---
+++ Start-Finance-master/InstaRichie/Views/AppointmentsPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SQLite;
+using StartFinance.Models;
+using Windows.UI.Popups;
+using SQLite.Net;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace StartFinance.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppointmentsPage : Page
+    {
+        SQLiteConnection conn; // adding an SQLite connection
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
+
+        public AppointmentsPage()
+        {
+            this.InitializeComponent();
+
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            /// Initializing a database
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            // Creating table
+            conn.CreateTable<Appointments>();
+            EventDate.Date = DateTime.Now; // gets current date and time
+
+      
+            
+            //Results();
+
+        }
+        
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+         //   Results();
+        }
+
+        private async void AddAppointment(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void EditAppointment(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+
+
+}
