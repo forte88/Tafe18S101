b2390d2b3caa511eee703ea620f592ea03c88b66
Milan <miki_inter@live.com>
2018-04-16 14:22:01 +0930

    feat_ContactDetails. Page was made with ContactDetails.cs and ContactDetailsPage.xaml. All text boxes and buttons are made.

---
+++ Start-Finance-master/InstaRichie/Models/ContactDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    public class ContactDetails
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string CompanyPhone { get; set; }
+       
+    }
+}
